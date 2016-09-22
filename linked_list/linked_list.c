1cb77c1b978acb36f40477de774e6d4760953741
yangpeng-chn <yangpeng.chn@gmail.com>
2016-09-22 23:33:18 +0900

    update linked list

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 struct node
 {
@@ -63,7 +64,7 @@ void addNodeAtEnd(struct node** head_ref, int new_data)
 	new_node->data  = new_data;
 	
 	/* 3. This new node is going to be the last node, so make next 
-		  of it as NULL*/
+			of it as NULL*/
 	new_node->next = NULL;
 	
 	/* 4. If the Linked List is empty, then make the new node as head */
@@ -71,7 +72,7 @@ void addNodeAtEnd(struct node** head_ref, int new_data)
 		*head_ref = new_node;
 		return;
 	}  
-	  
+		
 	struct node *last = *head_ref;
 	/* 5. Else traverse till the last node */
 	while (last->next != NULL)
@@ -146,7 +147,7 @@ void deleteNodeByPos(struct node** head_ref, int pos){ //e.g: 123, pos: 1, resul
 	if (*head_ref == NULL)
 		return;
 
-   	// Store head node
+	// Store head node
 	struct node* temp = *head_ref;
 
 	// If head needs to be removed
@@ -189,11 +190,157 @@ int getCountUsingRecursion(struct node* list){
 	return 1 + getCountUsingRecursion(list->next);
 }
 
+//O(n) The idea it to first search x and y in given linked list. 
+//If any of them is not present, then return. While searching for x and y, keep track of current and previous pointers. 
+//First change next of previous pointers, then change next of current pointers
+void swapNodes(struct node **head_ref, int x, int y){
+	printf("%s%d,%d\n", "Entered swapNodes, values:", x, y);
+	// Nothing to do if x and y are same
+	if (x == y) return;
+ 
+	// Search for x (keep track of prevX and CurrX
+	struct node *prevX = NULL, *currX = *head_ref;
+	while (currX && currX->data != x)
+	{
+		prevX = currX;
+		currX = currX->next;
+	}
+ 
+	// Search for y (keep track of prevY and CurrY
+	struct node *prevY = NULL, *currY = *head_ref;
+	while (currY && currY->data != y)
+	{
+		prevY = currY;
+		currY = currY->next;
+	}
+ 
+	// If either x or y is not present, nothing to do
+	if (currX == NULL || currY == NULL)
+		return;
+ 
+	// If x is not head of linked list
+	if (prevX != NULL)
+		prevX->next = currY;
+	else // Else make y as new head
+		*head_ref = currY;  
+ 
+	// If y is not head of linked list
+	if (prevY != NULL)
+		prevY->next = currX;
+	else  // Else make x as new head
+		*head_ref = currX;
+ 
+	// Swap next pointers
+	struct node *temp = currY->next;
+	currY->next = currX->next;
+	currX->next  = temp;
+}
+
+int getNth(struct node* head, int index){
+	printf("%s%d\n", "Entered getNth, index:", index);
+	struct node* current = head;
+	for (int i = 0; i < index && current != NULL; ++i){
+		current = current->next;
+	}
+	if (current != NULL){
+		return current->data;
+	}
+	assert(0);
+}
+
+void printMiddle(struct node *head){
+	printf("%s\n","Entered printMiddle");
+	struct node *slow_ptr = head;
+    struct node *fast_ptr = head;
+    if (head!=NULL){
+        while (fast_ptr != NULL && fast_ptr->next != NULL)
+        {
+            fast_ptr = fast_ptr->next->next;
+            slow_ptr = slow_ptr->next;
+        }
+        printf("The middle element is %d\n\n", slow_ptr->data);
+    }
+}
+
+//Find n’th node from the end of a Linked List
+/*
+Method 1 (Use length of linked list)
+1) Calculate the length of Linked List. Let the length be len.
+2) Print the (len – n + 1)th node from the begining of the Linked List.
+*/
+void printNthFromLast1(struct node* head, int n){
+	printf("%s%d\n","Entered printNthFromLast1, n: ", n);
+
+	struct node* current = head;
+	int len = 0;
+	while(current != NULL){
+		len++;
+		current = current->next;
+	}
+	if (n > len || n < 1){
+		printf("invalid input or %d is greater than the no. of nodes in list\n\n", n);
+		return;
+	}
+
+	struct node* temp = head;
+	for( int i = 1; i < len - n + 1; i++)
+		temp = temp->next;
+
+	printf("The %dth node from last is %d\n\n", n, temp->data);
+}
+
+/*
+Method 2 (Use two pointers) 
+Maintain two pointers – reference pointer and main pointer. 
+Initialize both reference and main pointers to head. 
+First move reference pointer to n nodes from head. 
+Now move both pointers one by one until reference pointer reaches end. 
+Now main pointer will point to nth node from the end. Return main pointer.
+*/
+
+void printNthFromLast2(struct node* head, int n){
+	printf("%s%d\n","Entered printNthFromLast2, n: ", n);
+	if ( n < 1 ){
+		printf("n must be greater than 0\n\n");
+		return;
+	}
+	struct node* ref_ptr = head;
+	struct node* main_ptr = head;
+
+	for(int i = 0; i < n; i++){
+		if(ref_ptr != NULL)
+			ref_ptr = ref_ptr->next;
+		else{
+			printf("%d is greater than the no. of nodes in list\n\n", n); //can handle empty list as well
+			return;
+		}
+	}
+
+	while(ref_ptr != NULL){
+		main_ptr = main_ptr->next;
+		ref_ptr = ref_ptr->next;
+	}
+	printf("The %dth node from last is %d\n\n", n, main_ptr->data);
+}
+
+void deleteList(struct node** head_ref){
+	printf("%s\n","Entered deleteList");
+	struct node* current = *head_ref;
+	struct node* tmp = *head_ref;
+	while(current != NULL){
+		tmp = current;
+		current = current->next;
+		free(tmp);
+	}
+	*head_ref = NULL; //this line is needed
+}
+
 int main()
 {
 	struct node* head = NULL;
 	struct node* second = NULL;
 	struct node* third = NULL;
+	struct node* empty = NULL;
 
 	head = (struct node*)malloc(sizeof(struct node));
 	second = (struct node*)malloc(sizeof(struct node));
@@ -253,10 +400,23 @@ int main()
 	printList(head);
 	printListAddress(head);
 
-	reversePrintList(head);
+	swapNodes(&head, 1, 3);
+	printList(head);
+
+	printMiddle(head);
+
+	printNthFromLast1(head, 0);
+	printNthFromLast2(head, 4);
+	printNthFromLast2(head, 5);
+	printNthFromLast2(empty, 0);
 
 	printf("The length of Linked list is: %d\n", getCount(head));
 	printf("The length of Linked list is: %d\n", getCountUsingRecursion(head));
+	printf("The 3th of Linked list is: %d\n", getNth(head, 3));
+	reversePrintList(head);
+
+	deleteList(&head);
+	printList(head);
 	return 0;
 }
 
